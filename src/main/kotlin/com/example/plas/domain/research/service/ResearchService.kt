5b1a4b9b6ccd788bf9b5be465d4a970ed84d59eb
Ji-Ha <xodhkd36@naver.com>
2022-04-20 22:33:32 +0900

    refactor(research): 필요없는 page 확인 라인 수정

--- src/main/kotlin/com/example/plas/domain/research/service/ResearchService.kt
+++ src/main/kotlin/com/example/plas/domain/research/service/ResearchService.kt
@@ -1,13 +1,11 @@
 package com.example.plas.domain.research.service
 
-import com.example.plas.domain.account.repository.AccountRepository
 import com.example.plas.domain.research.entity.Research
 import com.example.plas.domain.research.entity.SendResearchDto
 import com.example.plas.domain.research.repository.ResearchRepository
 import com.example.plas.security.JwtProvider
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -20,7 +18,6 @@ class ResearchService(
 
     @Transactional(readOnly = true)
     fun findAllPages(page: Int, token: String): Page<SendResearchDto> {
-        val page = if (page === 0) 0 else page - 1 // page는 index 처럼 0부터 시작
         val account = jwtProvider.getAccountFromToken(token) ?: throw RuntimeException()
         return researchRepository.findAllByAccount(account, PageRequest.of(page, 10))
     }
