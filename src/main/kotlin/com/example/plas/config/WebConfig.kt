1461c51eff6887ba7c33dfa70cc489e76341887c
Ji-Ha <xodhkd36@naver.com>
2022-04-28 18:24:21 +0900

    feature(cors): cors 설정 추가

--- src/main/kotlin/com/example/plas/config/WebConfig.kt
+++ src/main/kotlin/com/example/plas/config/WebConfig.kt
@@ -4,16 +4,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-//@Configuration
+@Configuration
 class WebConfig: WebMvcConfigurer {
-
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("*")
             .maxAge(3000)
-//        registry.addMapping("/**")
-//            .allowedOrigins("https://plasproject.duckdns.org/")
-//            .allowedMethods("*")
     }
 }
