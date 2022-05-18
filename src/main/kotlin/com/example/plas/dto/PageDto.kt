2a77c9572da3dc3d66c57a6204b741645c28c641
Ji-Ha <xodhkd36@naver.com>
2022-05-18 20:42:30 +0900

    refactor(pageDto): pagenation에서 totalPages 추가

--- src/main/kotlin/com/example/plas/dto/PageDto.kt
+++ src/main/kotlin/com/example/plas/dto/PageDto.kt
@@ -4,8 +4,9 @@ import org.springframework.data.domain.Page
 
 data class PageDto<T>(
     val content: List<T>,
-    val totalElements: Long
+    val totalElements: Long,
+    val totalPages: Int
 )
 {
-    constructor(page: Page<T>): this(page.content, page.totalElements)
+    constructor(page: Page<T>): this(page.content, page.totalElements, page.totalPages)
 }
